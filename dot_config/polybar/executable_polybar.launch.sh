d0f7a9ee503bae6c638d90f751d4190aae4dfde1
Twchy <rptech1402@gmail.com>
2024-05-03 15:51:50 +0100

    removed polybar and i3status

--- dot_config/polybar/executable_polybar.launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-killall -q polybar # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done # Launch: 'top' is the name of my Polybar
-polybar &
